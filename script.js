b3166d514af29b5d84661b36f29127a95b155ff0
07-muhammad <hibaumer20@gmail.com>
2025-05-29 15:41:09 +0500

    Update script.js
    First the problem was the menu button was opening when click but not closing... It is fixed now :)

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const toggleBtn = document.getElementById('menu-toggle');
+const toggleCloseBtn = document.getElementById('close-toggle');
 const nav = document.getElementById('nav');
 
 toggleBtn.addEventListener('click', () => {
-  nav.classList.toggle('open');
+  nav.classList.add('open');
+});
+
+toggleCloseBtn.addEventListener('click', () => {
+  nav.classList.remove('open');
 });
