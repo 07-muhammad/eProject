e8a279126003c4f0530a8c1a9e2e0fca783c0cc1
Muhammad <hibaumer20@gmail.com>
2025-06-11 18:07:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,42 @@ toggleBtn.addEventListener('click', () => {
 toggleCloseBtn.addEventListener('click', () => {
   nav.classList.remove('open');
 });
+
+
+let decorProducts = [
+  {
+    id: "p1",
+    Name: "Cherry Blossom",
+    category: "Flowers",
+    image: "img1.jpg",
+    des: "Deligate Petals and Graceful Bloom"
+  },
+  {
+    id: "p2",
+    Name: "Mr. Wotton",
+    category: "Wedding",
+    image: "img3.jpg",
+    des: "Noble Person, Shines Forever"
+  },
+  {
+    id: "p3",
+    Name: "Golden Decoration",
+    category: "Enggagement",
+    image: "img2.jpg",
+    des: "We Also Serves.."
+  },
+  {
+    id: "p4",
+    Name: "Happy Enggaged",
+    category: "Birthday",
+    image: "img4.jpg",
+    des: "Mr. & Mrs. Burlesdon"
+  },
+  {
+    id: "p5",
+    Name: "Our Rose Decors",
+    category: "Rose Flower",
+    image: "img5.jpg",
+    des: "Fully Decorated Hall"
+  }
+];
